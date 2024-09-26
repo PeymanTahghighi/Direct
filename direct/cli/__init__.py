d262d03e699c3b0d87759bc3a35f5e410ca65e47
Peyman Tahghighi <tahghighipeyman@gmail.com>
2024-09-26 11:59:20 -0600

    added missing file fixed minor issue

--- direct/cli/__init__.py
+++ direct/cli/__init__.py
@@ -29,7 +29,7 @@ def main():
     # Data related comments.
     register_upload_subcommand(root_subparsers)
 
-    args = root_parser.parse_args(['train', '--cfg', 'projects\\cvpr2022_recurrentvarnet\\fastmri\\AXT1_brain\\configs\\base_varnet.yaml'])
+    args = root_parser.parse_args(['train', '--cfg', 'projects/cvpr2022_recurrentvarnet/fastmri/AXT1_brain/configs/base_varnet.yaml'])
     args.subcommand(args)
 
 
